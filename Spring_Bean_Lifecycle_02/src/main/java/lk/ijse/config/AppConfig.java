99033d2474196fac44e8143a4f4abd1158806a4e
Lihini Apsara <apsaralihini11@gmail.com>
2025-01-23 09:12:50 +0530

    h

---
+++ Spring_Bean_Lifecycle_02/src/main/java/lk/ijse/config/AppConfig.java
@@ -0,0 +1,14 @@
+package lk.ijse.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"lk.ijse.bean"})
+public class AppConfig {
+    public AppConfig() {
+        System.out.println("App Config Constructor");
+    }
+
+
+}
